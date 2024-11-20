bea8a5296c0ace99bf72378fe29ddbf01403b135
estyg100 <estygrunwald1@gmail.com>
2024-11-20 17:01:33 -0500

    properties

--- bizObject.cs
+++ bizObject.cs
@@ -1,10 +1,13 @@
 ﻿using System.Data.SqlClient;
 using System.Data;
 using System.Diagnostics;
+using System.Reflection;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace CPUFramework
 {
-    public class bizObject
+    public class bizObject : INotifyPropertyChanged
     {
 
         string _tablename = "";
@@ -14,15 +17,24 @@ namespace CPUFramework
         string _primarykeyname = "";
         string _primarykeyparamname = "";
         DataTable _datatable = new();
+        List<PropertyInfo> _properties = new();
 
-        public bizObject(string tablename)
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public bizObject()
         {
-            _tablename = tablename;
-            _getsproc = tablename + "Get";
-            _updatesproc = tablename + "Update";
-            _deletesproc = tablename + "Delete";
-            _primarykeyname = tablename + "Id";
+            Type t = this.GetType();
+            _tablename = t.Name;
+            if (_tablename.ToLower().StartsWith("biz"))
+            {
+                _tablename = _tablename.Substring(3);
+            }
+            _getsproc = _tablename + "Get";
+            _updatesproc = _tablename + "Update";
+            _deletesproc = _tablename + "Delete";
+            _primarykeyname = _tablename + "Id";
             _primarykeyparamname = "@" + _primarykeyname;
+            _properties = t.GetProperties().ToList<PropertyInfo>();
         }
 
         public DataTable Load(int primarykeyvalue)
@@ -31,8 +43,52 @@ namespace CPUFramework
             SqlCommand cmd = SQLUtility.GetSqlCommand(_getsproc);
             SQLUtility.SetParamValue(cmd, _primarykeyparamname, primarykeyvalue);
             dt = SQLUtility.GetDataTable(cmd);
+            if (dt.Rows.Count > 0)
+            {
+                LoadProps(dt.Rows[0]);
+            }
             _datatable = dt;
             return dt;
+        }
+
+        private void LoadProps(DataRow dr)
+        {
+            foreach (DataColumn col in dr.Table.Columns)
+            {
+                string colname = col.ColumnName.ToLower();
+                PropertyInfo? prop = _properties.FirstOrDefault(p => p.Name.ToLower() == colname && p.CanWrite == true);
+                if (prop != null)
+                {
+                    prop.SetValue(this, dr[colname]);
+                }
+            }
+        }
+
+        public void Save()
+        {
+            SqlCommand cmd = SQLUtility.GetSqlCommand(_updatesproc);
+            foreach (SqlParameter param in cmd.Parameters)
+            {
+                string colname = param.ParameterName.ToLower().Substring(1);
+                PropertyInfo? prop = _properties.FirstOrDefault(p => p.Name.ToLower() == colname && p.CanRead == true);
+                if (prop != null)
+                {
+                    param.Value = prop.GetValue(this);
+                }
+            }
+            SQLUtility.ExecuteSQL(cmd);
+            foreach (SqlParameter param in cmd.Parameters)
+            {
+                if (param.Direction == ParameterDirection.InputOutput)
+                {
+                    string colname = param.ParameterName.ToLower().Substring(1);
+                    PropertyInfo? prop = _properties.FirstOrDefault(p => p.Name.ToLower() == colname && p.CanRead == true);
+                    if (prop != null)
+                    {
+                        prop.SetValue(this, param.Value);
+                    }
+                }
+            }
         }
 
         public void Delete(DataTable dt)
@@ -52,6 +108,11 @@ namespace CPUFramework
             DataRow r = dt.Rows[0];
             SQLUtility.SaveDataRow(r, _updatesproc);
             Debug.Print("-------------------------------");
+        }
+
+        protected void InvokePropertyChanged([CallerMemberName] string propertyname = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyname));
         }
     }
 }
